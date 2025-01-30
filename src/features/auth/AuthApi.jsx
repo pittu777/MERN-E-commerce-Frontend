0d7e39fb5dc68a2cd47ad6b594a38d255bb18342
pittu777 <pittuprasanth98@gmail.com>
2025-01-30 21:02:50 +0530

    first commit

---
+++ src/features/auth/AuthApi.jsx
@@ -0,0 +1,70 @@
+import {axiosi} from '../../config/axios'
+
+export const signup=async(cred)=>{
+    console.log("cre", cred);
+
+    try {
+        console.log(`Making POST request to: ${axiosi.defaults.baseURL}/auth/signup`);
+        const res=await axiosi.post("auth/signup",cred)
+        return res.data
+    } catch (error) {
+        console.error("Error during signup:", error.response.data);
+        throw error.response.data
+    }
+}
+export const login=async(cred)=>{
+    try {
+        const res=await axiosi.post("auth/login",cred)
+        return res.data
+    } catch (error) {
+        throw error.response.data
+    }
+}
+export const verifyOtp=async(cred)=>{
+    try {
+        const res=await axiosi.post("auth/verify-otp",cred)
+        return res.data
+    } catch (error) {
+        throw error.response.data
+    }
+}
+export const resendOtp=async(cred)=>{
+    try {
+        const res=await axiosi.post("auth/resend-otp",cred)
+        return res.data
+    } catch (error) {
+        throw error.response.data
+    }
+}
+export const forgotPassword=async(cred)=>{
+    try {
+        const res=await axiosi.post("auth/forgot-password",cred)
+        return res.data
+    } catch (error) {
+        throw error.response.data
+    }
+}
+export const resetPassword=async(cred)=>{
+    try {
+        const res=await axiosi.post("auth/reset-password",cred)
+        return res.data
+    } catch (error) {
+        throw error.response.data
+    }
+}
+export const checkAuth=async(cred)=>{
+    try {
+        const res=await axiosi.get("auth/check-auth")
+        return res.data
+    } catch (error) {
+        throw error.response.data
+    }
+}
+export const logout=async()=>{
+    try {
+        const res=await axiosi.get("auth/logout")
+        return res.data
+    } catch (error) {
+        throw error.response.data
+    }
+}
